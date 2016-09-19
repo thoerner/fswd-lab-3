78c0c6fa6d42965a7cae17e055900ced42dfd258
Tim Hoerner <thoerner@gmail.com>
2016-09-19 19:39:35 -0400

    convert to pug

--- public/app.js
+++ public/app.js
@@ -11,35 +11,35 @@ $('body').show();
 
 $(function() {
 
-  $('button#add').click(function(e) {
-    // Make sure the form does not get submitted
-    e.preventDefault();
+    $('button#add').click(function(e) {
+        // Make sure the form does not get submitted
+        e.preventDefault();
 
-    // extract the current value of the todo
-    var newTodo = $('input[name=todo]').val();
+        // extract the current value of the todo
+        var newTodo = $('input[name=todo]').val();
 
-    // create a completely new (unattached)
-    // list item element (<li>)
-    var newListElement = $('<li></li>');
-    var newListElementButton = $('<button>x</button>');
-    newListElementButton.click(function(e) {
-      e.preventDefault();
+        // create a completely new (unattached)
+        // list item element (<li>)
+        var newListElement = $('<li></li>');
+        var newListElementButton = $('<button>x</button>');
+        newListElementButton.click(function(e) {
+            e.preventDefault();
 
-      newListElement.fadeOut(1000, function() {
-        newListElementButton.remove();
-      });
+            newListElement.fadeOut(1000, function() {
+                newListElementButton.remove();
+            });
+        });
+
+        // Set its text
+        newListElement.text(newTodo);
+        newListElement.append(newListElementButton);
+
+        // And add it to the end of the list
+        $('ul').append(newListElement);
+
+        // Then clear out the input element
+        $('input[name=todo]').val("");
     });
-
-    // Set its text
-    newListElement.text(newTodo);
-    newListElement.append(newListElementButton);
-
-    // And add it to the end of the list
-    $('ul').append(newListElement);
-
-    // Then clear out the input element
-    $('input[name=todo]').val("");
-  });
 
 
 });
